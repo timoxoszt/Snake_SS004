92a407233adc73080c5172a21821e1825f493f8a
Long - UIT - 20520625 <84081009+longtiu20@users.noreply.github.com>
2021-06-09 10:47:12 +0700

    Add files via upload

--- incld.hpp
+++ incld.hpp
@@ -5,3 +5,18 @@
 #include <cstdlib>
 #include <conio.h>
 using namespace std;
+
+struct Point{
+    int x,y;
+};
+
+void gotoxy( int column, int line )
+  {
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
+    );
+  }
